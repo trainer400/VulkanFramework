697da8321d4a29bd2650911841ba7877e97268e7
Matteo Pignataro <matteo.pignataro@skywarder.eu>
2024-02-17 19:18:08 +0100

    [Cube] Implemented FPS view

--- examples/cube/main.cpp
+++ examples/cube/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <framework/core/vulkan.h>
 #include <framework/window/window.h>
+#include <framework/utils/FPSCamera.h>
+#include <framework/core/uniformBuffer.h>
 
 using namespace std;
 using namespace framework;
@@ -15,6 +17,17 @@ shared_ptr<Pipeline> cubePipeline;
 unique_ptr<SwapChain> swapChain;
 unique_ptr<RenderPass> renderPass;
 unique_ptr<FrameBufferCollection> frameBufferCollection;
+FPSCamera camera{1, 45, 0.1f, 100.0f, {}};
+
+struct GlobalUniformBuffer
+{
+    alignas(16) glm::mat4 model;
+    alignas(16) glm::mat4 view;
+    alignas(16) glm::mat4 projection;
+};
+
+// Uniform buffer
+shared_ptr<UniformBuffer<GlobalUniformBuffer>> gubo;
 
 class Cube : public DrawableElement
 {
@@ -28,7 +41,7 @@ public:
         vertices.insert(vertices.end(), {-1, 1, 0, 0, 0, 1});
         vertices.insert(vertices.end(), {1, 1, 0, 0, 1, 0});
 
-        indices.insert(indices.end(), {0, 1, 2, 1, 2, 3});
+        indices.insert(indices.end(), {0, 1, 2, 3, 2, 1});
 
         // clang-format on
     }
@@ -48,8 +61,20 @@ void createGraphicsObjects()
     shaders.push_back(vertex);
     shaders.push_back(fragment);
 
+    // Create the uniform buffer
+    UniformBufferConfiguration guboConfig;
+    guboConfig.bindingIndex = 0;
+    guboConfig.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    gubo = make_shared<UniformBuffer<GlobalUniformBuffer>>(lDevice, guboConfig);
+
+    // Create the descriptor set
+    std::vector<shared_ptr<DescriptorElement>> elements;
+    elements.push_back(gubo);
+
+    unique_ptr<DescriptorSet> descriptor = make_unique<DescriptorSet>(lDevice, elements);
+
     // Create the drawable collection
-    unique_ptr<DrawableCollection> cubeCollection = make_unique<DrawableCollection>(lDevice, nullptr, commandPool->getCommandPool(), shaders);
+    unique_ptr<DrawableCollection> cubeCollection = make_unique<DrawableCollection>(lDevice, move(descriptor), commandPool->getCommandPool(), shaders);
     cubeCollection->addElement(make_shared<Cube>());
     cubeCollection->addAttribute(DrawableAttribute::F3);
     cubeCollection->addAttribute(DrawableAttribute::F3);
@@ -57,15 +82,35 @@ void createGraphicsObjects()
 
     // Create the pipeline
     PipelineConfiguration config{};
-    config.cullMode = VK_CULL_MODE_NONE;
     cubePipeline = make_shared<Pipeline>(lDevice, move(cubeCollection), renderPass->getDepthTestType(), renderPass->getRenderPass(), config);
 
     // Add the pipeline to the renderer
     vulkan->addPipeline(cubePipeline);
 }
 
+void updateUniformBuffer()
+{
+    GlobalUniformBuffer buf{};
+
+    // Set the gubo depending on the camera
+    buf.view = camera.getLookAtMatrix();
+    buf.model = glm::mat4(1.0f);
+    buf.projection = camera.getPerspectiveMatrix(window->getWidth(), window->getHeight());
+
+    // Copy the gubo in memory
+    gubo->setData(buf);
+}
+
 void onUpdate()
 {
+    camera.updatePosition();
+    updateUniformBuffer();
+
+    // Set the cursor in position
+    glfwSetInputMode(window->getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPos(window->getWindow(), window->getWidth() / 2, window->getHeight() / 2);
+
+    // Draw the frame
     vulkan->draw({0.0f, 0.0f, 0.0f, 1.0f});
 }
 
@@ -116,6 +161,9 @@ int main()
 
     // Create imgui
     vulkan->setupImGui(window->getWindow(), [&]() {});
+
+    // Set the camera bindings
+    camera.registerCallbacks(window);
 
     // Run the window
     window->run([=]()
--- examples/cube/shaders/cube.vert
+++ examples/cube/shaders/cube.vert
@@ -1,5 +1,12 @@
 #version 450
 
+layout(binding = 0) uniform globalUniformBuffer
+{
+    mat4 model;
+    mat4 view;
+    mat4 projection;
+} gubo;
+
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
 
@@ -7,6 +14,6 @@ layout(location = 0) out vec3 outColor;
 
 void main()
 {
-    gl_Position = vec4(inPosition, 1.0f);
+    gl_Position = gubo.projection * gubo.view * gubo.model * vec4(inPosition, 1.0f);
     outColor = inColor;
 }

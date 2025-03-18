a836a76b09b2ddb7ebfd99ac53a719c03ac70c1b
Matteo Pignataro <matteo.pignataro@mail.polimi.it>
2025-03-18 16:21:52 +0100

    [ObjectWithMaterial] Added material index to parsing as well as standard vertex/index parsing

--- framework/utils/objectWithMaterialParser.cpp
+++ framework/utils/objectWithMaterialParser.cpp
@@ -55,5 +55,65 @@ namespace framework
         {
             throw std::runtime_error("[ObjectParser] Error from tiny-OBJ: " + warn + err);
         }
+
+        // The last +1 is for the material index (TODO use uint for material index)
+        vertexSize = 3 + (config.hasTexture ? 2 : 0) + (config.hasNormals ? 3 : 0) + (config.addMedians ? 3 : 0) + 1;
+
+        for (const auto &shape : shapes)
+        {
+            for (size_t i = 0; i < shape.mesh.indices.size(); i++)
+            {
+                const tinyobj::index_t &index = shape.mesh.indices[i];
+
+                // Insert the vertex
+                vertices.push_back((config.rightHandedRef ? -1 : 1) * attrib.vertices[3 * index.vertex_index + 0] * config.multiplicationFactor);
+                vertices.push_back(attrib.vertices[3 * index.vertex_index + 1] * config.multiplicationFactor);
+                vertices.push_back(attrib.vertices[3 * index.vertex_index + 2] * config.multiplicationFactor);
+
+                if (config.hasTexture)
+                {
+                    // Insert the texture coordinates
+                    vertices.push_back(attrib.texcoords[2 * index.texcoord_index + 0]);
+                    vertices.push_back(attrib.texcoords[2 * index.texcoord_index + 1]);
+                }
+
+                if (config.hasNormals)
+                {
+                    // Insert the normal coordinates
+                    vertices.push_back((config.rightHandedRef ? -1 : 1) * attrib.normals[3 * index.normal_index + 0]);
+                    vertices.push_back(attrib.normals[3 * index.normal_index + 1]);
+                    vertices.push_back(attrib.normals[3 * index.normal_index + 2]);
+                }
+
+                if (config.addMedians)
+                {
+                    // Insert point with 1 on the axis of this index % 3
+                    std::vector<float> med;
+                    med.insert(med.end(), {0, 0, 0});
+
+                    // Total length is 1 and the position depends on the vertex in the triangle
+                    med[indices.size() % 3] = 1.0f;
+
+                    // Insert the median inside the vertices
+                    vertices.push_back(med[0]);
+                    vertices.push_back(med[1]);
+                    vertices.push_back(med[2]);
+                }
+
+                // Insert the material index inside the vertex (/3 because it is the same for every vertex in the same triangle)
+                vertices.push_back(shape.mesh.material_ids[i / 3]);
+
+                // Insert the index
+                indices.push_back((vertices.size() / vertexSize) - 1);
+
+                // If just finished to insert a triangle, when it is a right handed reference, swap the indices
+                if (config.rightHandedRef && indices.size() % 3 == 0)
+                {
+                    uint32_t temp = indices[indices.size() - 1];
+                    indices[indices.size() - 1] = indices[indices.size() - 2];
+                    indices[indices.size() - 2] = temp;
+                }
+            }
+        }
     }
 }

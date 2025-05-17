ffcd1d708cb77f10c053bbaa5b7e3f79e1e846f7
Matteo Pignataro <matteo.pignataro@mail.polimi.it>
2025-05-17 07:15:38 +0200

    [DescriptorSet] refactored with snake case variables

--- framework/core/descriptorSet.cpp
+++ framework/core/descriptorSet.cpp
@@ -4,9 +4,9 @@
 
 namespace framework
 {
-    DescriptorSet::DescriptorSet(const std::shared_ptr<LogicalDevice> &lDevice, const std::vector<std::shared_ptr<DescriptorElement>> &elements)
+    DescriptorSet::DescriptorSet(const std::shared_ptr<LogicalDevice> &l_device, const std::vector<std::shared_ptr<DescriptorElement>> &elements)
     {
-        if (lDevice == nullptr)
+        if (l_device == nullptr)
         {
             throw std::runtime_error("[DescriptorSet] Null logical device instance");
         }
@@ -17,16 +17,16 @@ namespace framework
         }
 
         // TODO add check that all the bindings are progressive and consistent and not overlapping
-        this->lDevice = lDevice;
+        this->l_device = l_device;
 
         // Copy all the config data structures
-        std::vector<VkDescriptorPoolSize> poolSizes{elements.size()};
+        std::vector<VkDescriptorPoolSize> pool_sizes{elements.size()};
         std::vector<VkDescriptorSetLayoutBinding> bindings{elements.size()};
         std::vector<VkWriteDescriptorSet> descriptorWrites{elements.size()};
 
         for (int i = 0; i < elements.size(); i++)
         {
-            poolSizes[i] = elements[i]->getPoolSize();
+            pool_sizes[i] = elements[i]->getPoolSize();
             bindings[i] = elements[i]->getDescriptorSetLayoutBinding();
             descriptorWrites[i] = elements[i]->getWriteDescriptorSet();
         }
@@ -35,11 +35,11 @@ namespace framework
         VkDescriptorPoolCreateInfo poolInfo{};
 
         poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
-        poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
-        poolInfo.pPoolSizes = poolSizes.data();
+        poolInfo.poolSizeCount = static_cast<uint32_t>(pool_sizes.size());
+        poolInfo.pPoolSizes = pool_sizes.data();
         poolInfo.maxSets = 1;
 
-        if (vkCreateDescriptorPool(lDevice->getDevice(), &poolInfo, nullptr, &pool) != VK_SUCCESS)
+        if (vkCreateDescriptorPool(l_device->getDevice(), &poolInfo, nullptr, &pool) != VK_SUCCESS)
         {
             throw std::runtime_error("[DescriptorPool] Impossible to create descriptor pool");
         }
@@ -50,7 +50,7 @@ namespace framework
         layoutInfo.bindingCount = static_cast<uint32_t>(bindings.size());
         layoutInfo.pBindings = bindings.data();
 
-        if (vkCreateDescriptorSetLayout(lDevice->getDevice(), &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS)
+        if (vkCreateDescriptorSetLayout(l_device->getDevice(), &layoutInfo, nullptr, &descriptor_set_layout) != VK_SUCCESS)
         {
             throw std::runtime_error("[UniformBuffer] Error creating descriptor set layout");
         }
@@ -61,9 +61,9 @@ namespace framework
         allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
         allocInfo.descriptorPool = pool;
         allocInfo.descriptorSetCount = 1;
-        allocInfo.pSetLayouts = &descriptorSetLayout;
+        allocInfo.pSetLayouts = &descriptor_set_layout;
 
-        if (vkAllocateDescriptorSets(lDevice->getDevice(), &allocInfo, &descriptorSet) != VK_SUCCESS)
+        if (vkAllocateDescriptorSets(l_device->getDevice(), &allocInfo, &descriptor_set) != VK_SUCCESS)
         {
             throw std::runtime_error("[UniformBuffer] Error creating the descriptor set");
         }
@@ -71,22 +71,22 @@ namespace framework
         // Update the descriptor set inside the writes structs
         for (int i = 0; i < descriptorWrites.size(); i++)
         {
-            descriptorWrites[i].dstSet = descriptorSet;
+            descriptorWrites[i].dstSet = descriptor_set;
         }
 
-        vkUpdateDescriptorSets(lDevice->getDevice(), static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
+        vkUpdateDescriptorSets(l_device->getDevice(), static_cast<uint32_t>(descriptorWrites.size()), descriptorWrites.data(), 0, nullptr);
     }
 
     DescriptorSet::~DescriptorSet()
     {
         if (pool != VK_NULL_HANDLE)
         {
-            vkDestroyDescriptorPool(lDevice->getDevice(), pool, nullptr);
+            vkDestroyDescriptorPool(l_device->getDevice(), pool, nullptr);
         }
 
-        if (descriptorSetLayout != VK_NULL_HANDLE)
+        if (descriptor_set_layout != VK_NULL_HANDLE)
         {
-            vkDestroyDescriptorSetLayout(lDevice->getDevice(), descriptorSetLayout, nullptr);
+            vkDestroyDescriptorSetLayout(l_device->getDevice(), descriptor_set_layout, nullptr);
         }
     }
 
--- framework/core/descriptorSet.h
+++ framework/core/descriptorSet.h
@@ -11,20 +11,20 @@ namespace framework
     class DescriptorSet
     {
     public:
-        DescriptorSet(const std::shared_ptr<LogicalDevice> &lDevice, const std::vector<std::shared_ptr<DescriptorElement>> &elements);
+        DescriptorSet(const std::shared_ptr<LogicalDevice> &l_device, const std::vector<std::shared_ptr<DescriptorElement>> &elements);
         ~DescriptorSet();
 
         // Getters
         inline const VkDescriptorPool &getDescriptorPool() { return pool; }
-        inline const VkDescriptorSet &getDescriptorSet() { return descriptorSet; }
-        inline const VkDescriptorSetLayout &getDescriptorSetLayout() { return descriptorSetLayout; }
+        inline const VkDescriptorSet &getDescriptorSet() { return descriptor_set; }
+        inline const VkDescriptorSetLayout &getDescriptorSetLayout() { return descriptor_set_layout; }
 
     private:
         VkDescriptorPool pool = VK_NULL_HANDLE;
-        VkDescriptorSet descriptorSet = VK_NULL_HANDLE;
-        VkDescriptorSetLayout descriptorSetLayout = VK_NULL_HANDLE;
+        VkDescriptorSet descriptor_set = VK_NULL_HANDLE;
+        VkDescriptorSetLayout descriptor_set_layout = VK_NULL_HANDLE;
 
         // Framework objects
-        std::shared_ptr<LogicalDevice> lDevice;
+        std::shared_ptr<LogicalDevice> l_device;
     };
 }

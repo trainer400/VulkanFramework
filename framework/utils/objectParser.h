e63bc7a14d720b0e234d8d61d6d624707b1b3dfd
Matteo Pignataro <matteo.pignataro@mail.polimi.it>
2025-05-25 12:35:42 +0200

    [ObjectParser] Refactored with snake case variables

--- framework/utils/objectParser.h
+++ framework/utils/objectParser.h
@@ -12,23 +12,23 @@ namespace framework
 {
     struct ObjectParserConfiguration
     {
-        bool hasTexture = true;
-        bool hasNormals = true;
-        bool rightHandedRef = true;
-        bool addMedians = false;
-        bool invertTexture = false;
-        float multiplicationFactor = 1.0f;
+        bool has_texture = true;
+        bool has_normals = true;
+        bool right_handed_ref = true;
+        bool add_medians = false;
+        bool invert_texture = false;
+        float multiplication_factor = 1.0f;
     };
 
     /**
      * @brief The method parses an object file with the passed configuration.
-     * The user can decide to include textures/normals, to scale the model and 
+     * The user can decide to include textures/normals, to scale the model and
      * eventually to add medians to it. The method is also tolerant to different
-     * texture mapping formats, having a flag named "invertTexture". 
-     * 
+     * texture mapping formats, having a flag named "invertTexture".
+     *
      * @param tex_paths is a user vector that is modified by the method to include the texture names in the correct order.
      * @warning The texture ordering is the same which is then used by every vertex inside memory, so it is important
      * to keep the initial object ordering.
      */
-    std::vector<std::shared_ptr<DefaultDrawableElement>> parseObjFile(const char *filename, const ObjectParserConfiguration &config, std::vector<std::string>& tex_paths);
+    std::vector<std::shared_ptr<DefaultDrawableElement>> parseObjFile(const char *filename, const ObjectParserConfiguration &config, std::vector<std::string> &tex_paths);
 }
